ee27a626b0ef5c788ac81e76e0fcbe78d7510cbb
Sam Selikoff <sam.selikoff@gmail.com>
2022-02-09 11:30:00 -0500

    End of video

--- src/App.js
+++ src/App.js
@@ -1,3 +1,32 @@
+import { useState, useSyncExternalStore } from "react";
+
 export default function App() {
-  return <div>Hello!</div>;
+  let [showing, setShowing] = useState(true);
+
+  return (
+    <>
+      <button onClick={() => setShowing(!showing)}>toggle</button>
+      {showing && <Component />}
+      <Component />
+    </>
+  );
+}
+
+let now = new Date().toLocaleTimeString();
+setInterval(() => {
+  now = new Date().toLocaleTimeString();
+  notifiers.forEach((notify) => notify());
+}, 1000);
+
+let notifiers = new Set();
+function subscribe(notify) {
+  notifiers.add(notify);
+
+  return () => notifiers.delete(notify);
+}
+
+function Component() {
+  let store = useSyncExternalStore(subscribe, () => now);
+
+  return <p>The time is: {store}</p>;
 }
